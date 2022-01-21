d99295c208709722fcd7ec6d0b6c6d7e1a07ecf3
santapa <wipitech007@gmail.com>
2022-01-21 10:49:13 +0800

    update api port

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ app = init_app({
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
